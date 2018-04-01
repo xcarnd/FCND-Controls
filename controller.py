7c13f37275957e7e7b65dbddff3917a62fe46c45
陈柱辉 <chenzhuhui@idaoben.com>
2018-04-02 00:21:31 +0800

    Just reformat the file.

--- controller.py
+++ controller.py
@@ -15,11 +15,12 @@ MOI = np.array([0.005, 0.005, 0.01])
 MAX_THRUST = 10.0
 MAX_TORQUE = 1.0
 
+
 class NonlinearController(object):
 
     def __init__(self):
         """Initialize the controller object and control gains"""
-        return    
+        return
 
     def trajectory_control(self, position_trajectory, yaw_trajectory, time_trajectory, current_time):
         """Generate a commanded position, velocity and yaw based on the trajectory
@@ -36,22 +37,21 @@ class NonlinearController(object):
 
         ind_min = np.argmin(np.abs(np.array(time_trajectory) - current_time))
         time_ref = time_trajectory[ind_min]
-        
-        
+
         if current_time < time_ref:
             position0 = position_trajectory[ind_min - 1]
             position1 = position_trajectory[ind_min]
-            
+
             time0 = time_trajectory[ind_min - 1]
             time1 = time_trajectory[ind_min]
             yaw_cmd = yaw_trajectory[ind_min - 1]
-            
+
         else:
             yaw_cmd = yaw_trajectory[ind_min]
             if ind_min >= len(position_trajectory) - 1:
                 position0 = position_trajectory[ind_min]
                 position1 = position_trajectory[ind_min]
-                
+
                 time0 = 0.0
                 time1 = 1.0
             else:
@@ -60,16 +60,15 @@ class NonlinearController(object):
                 position1 = position_trajectory[ind_min + 1]
                 time0 = time_trajectory[ind_min]
                 time1 = time_trajectory[ind_min + 1]
-            
+
         position_cmd = (position1 - position0) * \
-                        (current_time - time0) / (time1 - time0) + position0
+                       (current_time - time0) / (time1 - time0) + position0
         velocity_cmd = (position1 - position0) / (time1 - time0)
-        
-        
+
         return (position_cmd, velocity_cmd, yaw_cmd)
-    
+
     def lateral_position_control(self, local_position_cmd, local_velocity_cmd, local_position, local_velocity,
-                               acceleration_ff = np.array([0.0, 0.0])):
+                                 acceleration_ff=np.array([0.0, 0.0])):
         """Generate horizontal acceleration commands for the vehicle in the local frame
 
         Args:
@@ -82,8 +81,9 @@ class NonlinearController(object):
         Returns: desired vehicle 2D acceleration in the local frame [north, east]
         """
         return np.array([0.0, 0.0])
-    
-    def altitude_control(self, altitude_cmd, vertical_velocity_cmd, altitude, vertical_velocity, attitude, acceleration_ff=0.0):
+
+    def altitude_control(self, altitude_cmd, vertical_velocity_cmd, altitude, vertical_velocity, attitude,
+                         acceleration_ff=0.0):
         """Generate vertical acceleration (thrust) command
 
         Args:
@@ -96,8 +96,7 @@ class NonlinearController(object):
         Returns: thrust command for the vehicle (+up)
         """
         return 0.0
-        
-    
+
     def roll_pitch_controller(self, acceleration_cmd, attitude, thrust_cmd):
         """ Generate the rollrate and pitchrate commands in the body frame
         
@@ -109,7 +108,7 @@ class NonlinearController(object):
         Returns: 2-element numpy array, desired rollrate (p) and pitchrate (q) commands in radians/s
         """
         return np.array([0.0, 0.0])
-    
+
     def body_rate_control(self, body_rate_cmd, body_rate):
         """ Generate the roll, pitch, yaw moment commands in the body frame
         
@@ -120,7 +119,7 @@ class NonlinearController(object):
         Returns: 3-element numpy array, desired roll moment, pitch moment, and yaw moment commands in Newtons*meters
         """
         return np.array([0.0, 0.0, 0.0])
-    
+
     def yaw_control(self, yaw_cmd, yaw):
         """ Generate the target yawrate
         
@@ -131,4 +130,3 @@ class NonlinearController(object):
         Returns: target yawrate in radians/sec
         """
         return 0.0
-    
